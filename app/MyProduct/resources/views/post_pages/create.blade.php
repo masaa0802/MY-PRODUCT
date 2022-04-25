0679cb11f9169f2609ab878fa510677d6e3f1c65
masaa0802 <masaakigoto0802@gmail.com>
2022-04-25 19:20:53 +0900

    修正

--- app/MyProduct/resources/views/post_pages/create.blade.php
+++ app/MyProduct/resources/views/post_pages/create.blade.php
@@ -15,48 +15,47 @@
                     <div>
                         <label for="video" class="mt-3">*ポートフォリオ動画</label>
                         <input type="file" id="video" name="video" value="{{ old('video') }}" class="form-control {{ $errors->has('video') ? 'is-invalid' : '' }}">
+                        @if ($errors->has('video'))
+                            <div class="invalid-feedback">
+                                {{ $errors->first('video') }}
+                            </div>
+                        @endif
                     </div>
-                    @if ($errors->has('video'))
-                        <div class="invalid-feedback">
-                            {{ $errors->first('video') }}
-                        </div>
-                    @endif
                     <div>
                         <label for="git_url" class="mt-3">*GitHubのURL</label>
                         <input name="git_url" value="{{ old('git_url') }}" class="form-control {{ $errors->has('git_url') ? 'is-invalid' : '' }}" placeholder="例)https://github.com/"/>
+                        @if ($errors->has('git_url'))
+                            <div class="invalid-feedback">
+                                {{ $errors->first('git_url') }}
+                            </div>
+                        @endif
                     </div>
-                    @if ($errors->has('git_url'))
-                        <div class="invalid-feedback">
-                            {{ $errors->first('git_url') }}
-                        </div>
-                    @endif
-
                     <div>
                         <label for="site_url" class="mt-3">サイトのURL(任意)</label>
                         <input name="site_url" value="{{ old('site_url') }}" class="form-control {{ $errors->has('site_url') ? 'is-invalid' : '' }}" placeholder="例)https://www.google.com"/>
+                        @if ($errors->has('site_url'))
+                            <div class="invalid-feedback">
+                                {{ $errors->first('site_url') }}
+                            </div>
+                        @endif
                     </div>
-                    @if ($errors->has('site_url'))
-                        <div class="invalid-feedback">
-                            {{ $errors->first('site_url') }}
-                        </div>
-                    @endif
                     <div>
                         <label for="title" class="mt-3">*タイトル</label>
                         <input name="title" value="{{ old('title') }}" class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}" placeholder="タイトルの入力欄"/>
+                        @if ($errors->has('title'))
+                            <div class="invalid-feedback">
+                                {{ $errors->first('title') }}
+                            </div>
+                        @endif
                     </div>
-                    @if ($errors->has('title'))
-                        <div class="invalid-feedback">
-                            {{ $errors->first('title') }}
-                        </div>
-                    @endif
                     <div>
                         <textarea name="body" class="mt-3 form-control {{ $errors->has('body') ? 'is-invalid' : '' }}" placeholder="内容の入力" style="height:200px;">{{ old('body') }}</textarea>
+                        @if ($errors->has('body'))
+                            <div class="invalid-feedback">
+                                {{ $errors->first('body') }}
+                            </div>
+                        @endif
                     </div>
-                    @if ($errors->has('body'))
-                        <div class="invalid-feedback">
-                            {{ $errors->first('body') }}
-                        </div>
-                    @endif
                     <button class="mt-3 btn btn-primary" type="submit">送信</button>
                 </form>
             @endif
